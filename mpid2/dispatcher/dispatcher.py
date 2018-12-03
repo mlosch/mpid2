4d9244f4a69aa9386c90e7a3a7a313d09ed8d3ba
Max Losch <mlosch@mpi-inf.mpg.de>
2018-12-03 19:02:00 +0100

    synchronize host assignment via queues and time reserving

--- mpid2/dispatcher/dispatcher.py
+++ mpid2/dispatcher/dispatcher.py
@@ -2,8 +2,10 @@ import subprocess
 import time
 import sys
 import datetime
+import Queue
 from functools import partial
 from random import shuffle, seed
+import multiprocessing as mp
 import os
 
 MAX_PARALLEL_JOBS = 4
@@ -17,6 +19,12 @@ MAX_RETRIES = 3
 At below what temperature is a gpu considered unused
 """
 MAX_TEMPERATURE = 40
+
+"""
+How much time in seconds does a host gets reserved by a job on startup, before allowing other jobs to take the same host
+"""
+RESERVE_TIME_FOR_JOB_STARTUP = 10
+
 
 LOG_TARGETS = dict(
 	none=None,
@@ -181,7 +189,24 @@ def find_free_host(hostlist, required_gpus, required_mem):
 	return None, []
 
 
-def _async_dispatch(task, hostlist, log_target):
+def __interrupt_safe_put(q, obj, timeout=0.1):
+	while True:
+		try:
+			q.put(obj, timeout=timeout)
+		except Queue.Full:
+			continue
+		return
+
+def __interrupt_safe_get(q, timeout=0.1):
+	while True:
+		try:
+			obj = q.get(timeout=timeout)
+		except Queue.Empty:
+			continue
+		return obj
+
+
+def _async_dispatch(task, queue_pending, queue_ready, log_target):
 	"""Dispatch helper loop. Do not call individually.
 
 	Loops randomly over all hosts and dispatches the given command if a host satisfies the given requirements on memory and number of gpus.
@@ -190,8 +215,10 @@ def _async_dispatch(task, hostlist, log_target):
 	----------
 	task : tuple
 		Tuple of length 4 (int, str, int, int), stating the command id, the command string, the number of required gpus and the amount of required free memory in Megabytes.
-	hostlist : list
-		List of hostnames or addresses
+	queue_pending : multiprocessing.Queue
+		Enqueues list of hostnames that have not been evaluated for utilization yet
+	queue_ready : multiprocessing.Queue
+		Enqueues list of hosts that satisfy gpu requirements and are ready to use
 	log_target : str
 		One of the keys in LOG_TARGETS 
 
@@ -206,15 +233,24 @@ def _async_dispatch(task, hostlist, log_target):
 	"""
 
 	seed(datetime.datetime.now())
-	shuffle(hostlist)  # shuffle to reduce risk of querying the same machines multiple times
+	#shuffle(hostlist)  # shuffle to reduce risk of querying the same machines multiple times
 
-	idx, command, required_gpus, required_mem = task
+	idx, command = task
 	dispatched = False
 	available_host = None
 	tries_left = MAX_RETRIES
 
 	while not dispatched:
-		available_host, gpuids = find_free_host(hostlist, required_gpus, required_mem)
+		#available_host, gpuids = find_free_host(hostlist, required_gpus, required_mem)
+		access_info = None
+		while access_info is None:
+			access_info = __interrupt_safe_get(queue_ready)
+			# if the information is greater than 5 seconds old, put back in pending queue
+			if (time.time() - access_info['t']) > 5.0:
+				__interrupt_safe_put(queue_pending, (access_info['hostname'], 0))
+
+		available_host, gpuids = access_info['hostname'], access_info['gpuids']
+
 		if available_host is not None:
 			dispatched = True
 
@@ -231,7 +267,11 @@ def _async_dispatch(task, hostlist, log_target):
 			_print_info('Dispatching command [%d] on host %s on gpus: %s' % (idx, available_host, ','.join(gpuids)))
 			t_start = time.time()
 			try:
+				 # disable this host for 10 seconds to allow enough time to reserve memory
+				__interrupt_safe_put(queue_pending, (available_host, t_start + RESERVE_TIME_FOR_JOB_STARTUP))
 				retcode, lastoutput = remote_exec(available_host, host_command, logfile)
+				# retcode = 0
+				# lastoutput = 'success'
 			except KeyboardInterrupt as e:
 				_print_error('Interrupted command [%d] on host %s on gpus: %s' % (idx, available_host, ','.join(gpuids)))
 				return None, None, None
@@ -268,6 +308,66 @@ def _async_dispatch(task, hostlist, log_target):
 			time.sleep(1)  # sleep 1 second
 	return available_host, gpuids, host_command
 
+
+def _utilization_enqueuer(queue_ready, queue_pending, required_gpus, required_mem):
+	conditions_satisfiable = False
+
+	# if not conditions_satisfiable:
+	# 	raise RuntimeError('Conditions based on %d required gpus with %dMB each is not satisfiable by any machine at any time.' % (required_gpus, required_mem))
+
+	while True:
+		host, twait = __interrupt_safe_get(queue_pending)
+
+		if host == '':
+			# We're done
+			break
+
+		if time.time() < twait:
+			# do not evaluate this host yet
+			__interrupt_safe_put(queue_pending, (host, twait))
+			continue
+
+		timestamp = time.time()
+		gpu_util = query_gpu_utilization(host)
+
+		if len(gpu_util) >= required_gpus:
+			# check for satisfiability
+			req_mem_satisfiable = 0
+			for mem_used, mem_total, temp in gpu_util:
+				if mem_total > required_mem:
+					req_mem_satisfiable += 1
+			if req_mem_satisfiable >= required_gpus:
+				conditions_satisfiable = True
+
+			queued_gpus = []
+			satisfied = False
+			for gpui, gpu in enumerate(gpu_util):
+				if gpu[2] > MAX_TEMPERATURE:
+					continue
+				if (gpu[1]-gpu[0]) >= required_mem:
+					queued_gpus.append(str(gpui))
+				if len(queued_gpus) == required_gpus:
+					satisfied = True
+					break
+
+			if satisfied:
+				try:
+					queue_ready.put({'t': timestamp, 'hostname': host, 'gpuids': queued_gpus}, timeout=0.1)
+				except Queue.Full as e:
+					# put back in pending queue, if queue_ready is full (that should not happen)
+					print('Unexpected Exception caught: %s' % str(e))
+					queue_pending.put((host, 0), timeout=0.1)
+			else:
+				# put back and check later in 5 seconds
+				__interrupt_safe_put(queue_pending, (host, time.time() + 5))
+		else:
+			# otherwise, leave the host out of our list entirely. It will never satisfy our requirements
+			print('never satisfies: %s'%host)
+			pass
+
+
+
+
 def dispatch(hostlist, commands, required_gpus=1, required_mem=8000, log_target='file', rm_failed_logs=False):
 	"""Main dispatcher method.
 
@@ -285,26 +385,40 @@ def dispatch(hostlist, commands, required_gpus=1, required_mem=8000, log_target=
 		One of the keys in LOG_TARGETS 
 	"""
 
-	import multiprocessing as mp
-
 	if type(required_gpus) is list and len(required_gpus) != len(commands):
 		raise RuntimeError('Entries in required_gpus list must be equal to entries in commands.')
 	if type(required_mem) is list and len(required_mem) != len(commands):
 		raise RuntimeError('Entries in required_mem list must be equal to entries in commands.')
 
-	if type(required_gpus) is int:
-		required_gpus = [required_gpus]*len(commands)
-	if type(required_mem) is int:
-		required_mem = [required_mem]*len(commands)
+	# if type(required_gpus) is int:
+	# 	required_gpus = [required_gpus]*len(commands)
+	# if type(required_mem) is int:
+	# 	required_mem = [required_mem]*len(commands)
 
 	
 	pool = mp.Pool(processes=MAX_PARALLEL_JOBS)
+	m = mp.Manager()
+
+	# fill queue
+	queue_pending = m.Queue(len(hostlist)+1)
+	queue_ready = m.Queue(len(hostlist)+1)
+	for host in hostlist:
+		queue_pending.put((host, 0))
+
+	# start enqueuer
+	enqueuer = mp.Process(target=_utilization_enqueuer, args=(queue_ready, queue_pending, required_gpus, required_mem))
+	enqueuer.start()
 
 	cmdinds = range(len(commands))
 	pool.map_async(
-		partial(_async_dispatch, hostlist=hostlist, log_target=log_target), 
-		zip(cmdinds, commands, required_gpus, required_mem)
+		partial(_async_dispatch, queue_pending=queue_pending, queue_ready=queue_ready, log_target=log_target), 
+		zip(cmdinds, commands)
 		).get(9999999)
 
+	print('Please wait for processes to finish...')
 	pool.close()
+	queue_pending.put(('', 0))
+
+	pool.join()
+	enqueuer.join()
 
